ef883e9761766e10b4b3af61079af6cd5af79f98
IgorZhyvotkov <105238566+IgorZhyvotkov@users.noreply.github.com>
2022-07-12 14:49:26 +0000

    Deploying to gh-pages from @ IgorZhyvotkov/team-project-group-12@b60024406148fead15a485a8d3c02b2745dac3ed 🚀

---
+++ index.36d5e071.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]");e.addEventListener("click",(function(){var n="true"===e.getAttribute("aria-expanded")||!1;e.classList.toggle("is-open"),e.setAttribute("aria-expanded",!n),t.classList.toggle("is-open"),document.body.classList.toggle("is-open")}))}();
+//# sourceMappingURL=index.36d5e071.js.map
