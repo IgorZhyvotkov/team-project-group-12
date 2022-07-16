6fbb214b38a03b1c51f8569f7e39c3a0363e684c
IgorZhyvotkov <105238566+IgorZhyvotkov@users.noreply.github.com>
2022-07-16 08:01:17 +0000

    Deploying to gh-pages from @ IgorZhyvotkov/team-project-group-12@90f0700f4ae3818a90fd60f4db5822cde658333e 🚀

---
+++ index.7cfc34d4.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".swiper",{direction:"horizontal",loop:!0,pagination:{el:".swiper-pagination",clickable:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},scrollbar:{el:".swiper-scrollbar"}});
+//# sourceMappingURL=index.7cfc34d4.js.map
