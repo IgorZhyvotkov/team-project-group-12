b52f56ca5827f257c763741f873b9786f663bd49
Osadchyi Yurii <osa26091994@icloud.com>
2022-07-17 15:39:13 +0300

    Add new branch

---
+++ dist/index.7284ef6a.js
@@ -0,0 +1,21 @@
+const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: true,
+    // If we need pagination
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    },
+    // Navigation arrows
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+    // And if we need scrollbar
+    scrollbar: {
+        el: ".swiper-scrollbar"
+    }
+});
+
+//# sourceMappingURL=index.7284ef6a.js.map
